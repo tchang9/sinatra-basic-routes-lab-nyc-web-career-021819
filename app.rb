0d09084b818d2481d05bd6509d419967656e584f
tchang9 <tlchang@gmail.com>
2019-03-10 13:41:46 -0400

    Sinatra basic routes

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is "
+    end
+
+    get '/hometown' do
+        "My hometown is "
+    end
+
+    get '/favorite-song' do
+        "My favorite song is "
+    end
 end
